b62cced545bf9ce835a5e800b7cc4d7078125271
Daniel Poverjuc <daniel.ungurean2@gmail.com>
2023-05-22 12:57:21 +0300

    Data Configuration Completed

--- Query/database.sql
+++ Query/database.sql
@@ -1,1 +1,80 @@
-﻿
+﻿USE MASTER;
+
+GO
+
+DROP DATABASE IF EXISTS LibraryDB;
+
+GO
+
+CREATE DATABASE LibraryDB;
+
+GO
+
+USE LibraryDB;
+
+GO
+
+-- TABLES
+
+CREATE TABLE Genre (
+	Id INT IDENTITY(1, 1) PRIMARY KEY,
+	"Name" VARCHAR(255) UNIQUE NOT NULL
+);
+
+GO
+
+INSERT INTO Genre ("Name")
+VALUES
+  ('Fiction'),
+  ('Mystery'),
+  ('Science Fiction'),
+  ('Romance'),
+  ('Fantasy'),
+  ('Science'),
+  ('Biology'),
+  ('History'),
+  ('Thriller'),
+  ('Horror'),
+  ('Poetry'),
+  ('Comedy'),
+  ('Self-Help'),
+  ('Business'),
+  ('Cooking'),
+  ('Travel'),
+  ('Art'),
+  ('Music'),
+  ('Sports'),
+  ('Biography');
+
+GO
+
+CREATE TABLE Book (
+	Id INT IDENTITY(1, 1) PRIMARY KEY,
+	Author VARCHAR(255) NOT NULL,
+	PublishingHouse VARCHAR(255) NOT NULL,
+	ReleaseYear INT NOT NULL, 
+	Price FLOAT NOT NULL,
+	Genre INT FOREIGN KEY REFERENCES Genre(Id) NOT NULL
+);
+
+GO
+
+CREATE TABLE Reader (
+	Id INT IDENTITY(1, 1) PRIMARY KEY,
+	"Name" VARCHAR(255) NOT NULL,
+	"Address" VARCHAR(255) NOT NULL,
+	PhoneNumber VARCHAR(15) NOT NULL
+);
+
+GO
+
+CREATE TABLE Rent (
+	Id INT IDENTITY(1, 1) PRIMARY KEY,
+	Book INT FOREIGN KEY REFERENCES Book(Id) NOT NULL,
+	Reader INT FOREIGN KEY REFERENCES Reader(Id) NOT NULL,
+	RentDate DATE NOT NULL,
+	ExpectedReturnDate DATE NOT NULL,
+	ReturnDate DATE
+);
+
+-- /TABLES
---
+++ Service/DataService.cs
@@ -0,0 +1,195 @@
+﻿using Library.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Service
+{
+    public class DataService
+    {
+        private readonly DbRepository db;
+
+        public DataService()
+        {
+            db = new DbRepository();
+        }
+
+        // Genre methods
+
+        public List<Genre> GetAllGenres()
+        {
+            return db.Genres.ToList();
+        }
+
+        // Book methods
+
+        public List<Book> GetAllBooks()
+        {
+            return db.Books.ToList();
+        }
+
+        // Reader methods
+
+        public List<Reader> GetAllReaders()
+        {
+            return db.Readers.ToList();
+        }
+
+        // Rent methods
+
+        public List<Rent> GetAllRents()
+        {
+            return db.Rents.ToList();
+        }
+
+        // Genre methods
+
+        public Genre FindGenreById(int id)
+        {
+            return db.Genres.Find(id);
+        }
+
+        public Genre FindGenreByName(string name)
+        {
+            return db.Genres.FirstOrDefault(g => g.Name == name);
+        }
+
+        public void AddGenre(Genre genre)
+        {
+            db.Genres.Add(genre);
+            db.SaveChanges();
+        }
+
+        public void UpdateGenre(Genre genre)
+        {
+            var existingGenre = db.Genres.Find(genre.Id);
+            if (existingGenre != null)
+            {
+                existingGenre.Name = genre.Name;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteGenre(int genreId)
+        {
+            var genre = db.Genres.Find(genreId);
+            if (genre != null)
+            {
+                db.Genres.Remove(genre);
+                db.SaveChanges();
+            }
+        }
+
+        // Book methods
+
+        public Book FindBookById(int id)
+        {
+            return db.Books.Find(id);
+        }
+
+        public void AddBook(Book book)
+        {
+            db.Books.Add(book);
+            db.SaveChanges();
+        }
+
+        public void UpdateBook(Book book)
+        {
+            var existingBook = db.Books.Find(book.Id);
+            if (existingBook != null)
+            {
+                existingBook.Author = book.Author;
+                existingBook.PublishingHouse = book.PublishingHouse;
+                existingBook.ReleaseYear = book.ReleaseYear;
+                existingBook.Price = book.Price;
+                existingBook.Genre = book.Genre;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteBook(int bookId)
+        {
+            var book = db.Books.Find(bookId);
+            if (book != null)
+            {
+                db.Books.Remove(book);
+                db.SaveChanges();
+            }
+        }
+
+        // Reader methods
+
+        public Reader FindReaderById(int id)
+        {
+            return db.Readers.Find(id);
+        }
+
+        public void AddReader(Reader reader)
+        {
+            db.Readers.Add(reader);
+            db.SaveChanges();
+        }
+
+        public void UpdateReader(Reader reader)
+        {
+            var existingReader = db.Readers.Find(reader.Id);
+            if (existingReader != null)
+            {
+                existingReader.Name = reader.Name;
+                existingReader.Address = reader.Address;
+                existingReader.PhoneNumber = reader.PhoneNumber;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteReader(int readerId)
+        {
+            var reader = db.Readers.Find(readerId);
+            if (reader != null)
+            {
+                db.Readers.Remove(reader);
+                db.SaveChanges();
+            }
+        }
+
+        // Rent methods
+
+        public Rent FindRentById(int id)
+        {
+            return db.Rents.Find(id);
+        }
+
+        public void AddRent(Rent rent)
+        {
+            db.Rents.Add(rent);
+            db.SaveChanges();
+        }
+
+        public void UpdateRent(Rent rent)
+        {
+            var existingRent = db.Rents.Find(rent.Id);
+            if (existingRent != null)
+            {
+                existingRent.Book = rent.Book;
+                existingRent.Reader = rent.Reader;
+                existingRent.RentDate = rent.RentDate;
+                existingRent.ExpectedReturnDate = rent.ExpectedReturnDate;
+                existingRent.ReturnDate = rent.ReturnDate;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteRent(int rentId)
+        {
+            var rent = db.Rents.Find(rentId);
+            if (rent != null)
+            {
+                db.Rents.Remove(rent);
+                db.SaveChanges();
+            }
+        }
+    }
+
+}
