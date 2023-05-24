a679fab100ed8ded23ce52cbd361f167d7a55fd1
Daniel Poverjuc <daniel.ungurean2@gmail.com>
2023-05-24 20:27:07 +0300

    Rent form completed.

--- Rents.Designer.cs
+++ Rents.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rentsDataGridView = new System.Windows.Forms.DataGridView();
+            this.bookLabel = new System.Windows.Forms.Label();
+            this.bookCB = new System.Windows.Forms.ComboBox();
+            this.readerCB = new System.Windows.Forms.ComboBox();
+            this.readerLabel = new System.Windows.Forms.Label();
+            this.rentDateLabel = new System.Windows.Forms.Label();
+            this.rentDateTB = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.updateBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.rentsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rentsDataGridView
+            // 
+            this.rentsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.rentsDataGridView.Location = new System.Drawing.Point(318, 12);
+            this.rentsDataGridView.Name = "rentsDataGridView";
+            this.rentsDataGridView.RowHeadersWidth = 51;
+            this.rentsDataGridView.RowTemplate.Height = 24;
+            this.rentsDataGridView.Size = new System.Drawing.Size(470, 426);
+            this.rentsDataGridView.TabIndex = 0;
+            this.rentsDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.rentsDataGridView_CellClick);
+            // 
+            // bookLabel
+            // 
+            this.bookLabel.AutoSize = true;
+            this.bookLabel.BackColor = System.Drawing.Color.Transparent;
+            this.bookLabel.Location = new System.Drawing.Point(30, 44);
+            this.bookLabel.Name = "bookLabel";
+            this.bookLabel.Size = new System.Drawing.Size(42, 16);
+            this.bookLabel.TabIndex = 19;
+            this.bookLabel.Text = "Book:";
+            // 
+            // bookCB
+            // 
+            this.bookCB.FormattingEnabled = true;
+            this.bookCB.Location = new System.Drawing.Point(156, 41);
+            this.bookCB.Name = "bookCB";
+            this.bookCB.Size = new System.Drawing.Size(141, 24);
+            this.bookCB.TabIndex = 21;
+            // 
+            // readerCB
+            // 
+            this.readerCB.FormattingEnabled = true;
+            this.readerCB.Location = new System.Drawing.Point(156, 77);
+            this.readerCB.Name = "readerCB";
+            this.readerCB.Size = new System.Drawing.Size(141, 24);
+            this.readerCB.TabIndex = 23;
+            // 
+            // readerLabel
+            // 
+            this.readerLabel.AutoSize = true;
+            this.readerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.readerLabel.Location = new System.Drawing.Point(30, 80);
+            this.readerLabel.Name = "readerLabel";
+            this.readerLabel.Size = new System.Drawing.Size(56, 16);
+            this.readerLabel.TabIndex = 22;
+            this.readerLabel.Text = "Reader:";
+            // 
+            // rentDateLabel
+            // 
+            this.rentDateLabel.AutoSize = true;
+            this.rentDateLabel.BackColor = System.Drawing.Color.Transparent;
+            this.rentDateLabel.Location = new System.Drawing.Point(30, 120);
+            this.rentDateLabel.Name = "rentDateLabel";
+            this.rentDateLabel.Size = new System.Drawing.Size(70, 16);
+            this.rentDateLabel.TabIndex = 24;
+            this.rentDateLabel.Text = "Rent Date:";
+            // 
+            // rentDateTB
+            // 
+            this.rentDateTB.Location = new System.Drawing.Point(156, 117);
+            this.rentDateTB.Name = "rentDateTB";
+            this.rentDateTB.Size = new System.Drawing.Size(141, 22);
+            this.rentDateTB.TabIndex = 25;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(189, 164);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(108, 42);
+            this.addBtn.TabIndex = 26;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(189, 164);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(108, 42);
+            this.updateBtn.TabIndex = 27;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // Rents
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.rentDateLabel);
+            this.Controls.Add(this.rentDateTB);
+            this.Controls.Add(this.readerCB);
+            this.Controls.Add(this.readerLabel);
+            this.Controls.Add(this.bookCB);
+            this.Controls.Add(this.bookLabel);
+            this.Controls.Add(this.rentsDataGridView);
+            this.Name = "Rents";
             this.Text = "Rents";
+            this.Load += new System.EventHandler(this.Rents_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rentsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView rentsDataGridView;
+        private System.Windows.Forms.Label bookLabel;
+        private System.Windows.Forms.ComboBox bookCB;
+        private System.Windows.Forms.ComboBox readerCB;
+        private System.Windows.Forms.Label readerLabel;
+        private System.Windows.Forms.Label rentDateLabel;
+        private System.Windows.Forms.TextBox rentDateTB;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button updateBtn;
     }
 }
--- Rents.cs
+++ Rents.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Library.Data;
+using Library.Service;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,179 @@ namespace Library
 {
     public partial class Rents : Form
     {
+        DataService service;
+        Rent toUpdate;
         public Rents()
         {
             InitializeComponent();
+            service = new DataService();
+            toUpdate = new Rent();
+        }
+
+        private void Rents_Load(object sender, EventArgs e)
+        {
+            showRents();
+        }
+
+        private void showAddBtn()
+        {
+            updateBtn.Hide();
+            addBtn.Show();
+        }
+
+        private void showUpdateBtn()
+        {
+            updateBtn.Show();
+            addBtn.Hide();
+        }
+
+        private void showRents()
+        {
+            rentsDataGridView.Columns.Clear();
+            bookCB.Items.Clear();
+            readerCB.Items.Clear();
+
+            foreach(Book book in service.GetAllBooks())
+            {
+                bookCB.Items.Add(book.Id);
+            }
+
+            foreach(Reader reader in service.GetAllReaders())
+            {
+                readerCB.Items.Add(reader.Id);
+            }
+
+            bookCB.Text = string.Empty;
+            readerCB.Text = string.Empty;
+            rentDateTB.Text = string.Empty;
+
+            var rents = (from r in service.GetAllRents()
+                         select new
+                         {
+                             r.Id,
+                             Book = r.Book1.Title + " by " + r.Book1.Author,
+                             Reader = r.Reader1.Name + " " + r.Reader1.Id,
+                             ReaderPhoneNumber = r.Reader1.PhoneNumber,
+                             r.RentDate,
+                             MustBeReturnedBefore = r.ExpectedReturnDate,
+                             r.ReturnDate
+                         });
+            rentsDataGridView.DataSource = rents.ToList();
+            rentsDataGridView.Columns[0].Visible = false;
+
+            rentsDataGridView.Columns.Add(new DataGridViewImageColumn()
+            {
+                Image = Library.Properties.Resources._return,
+                ImageLayout = DataGridViewImageCellLayout.Zoom,
+                HeaderText = "Return",
+                Name = "return"
+            });
+            rentsDataGridView.Columns.Add(new DataGridViewImageColumn()
+            {
+                Image = Library.Properties.Resources.update,
+                ImageLayout = DataGridViewImageCellLayout.Zoom,
+                HeaderText = "Update",
+                Name = "update"
+            });
+            rentsDataGridView.Columns.Add(new DataGridViewImageColumn()
+            {
+                Image = Library.Properties.Resources.delete,
+                ImageLayout = DataGridViewImageCellLayout.Zoom,
+                HeaderText = "Delete",
+                Name = "delete"
+            });
+        }
+
+        private DateTime calculateExpectedReturnDate(DateTime rentDate)
+        {
+            return rentDate.AddDays(14);
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Rent rent = new Rent()
+                {
+                    Book = Convert.ToInt32(bookCB.Text),
+                    Reader = Convert.ToInt32(readerCB.Text),
+                    RentDate = DateTime.Parse(rentDateTB.Text),
+                    ExpectedReturnDate = calculateExpectedReturnDate(DateTime.Parse(rentDateTB.Text)),
+                    ReturnDate = null
+                };
+
+                service.AddRent(rent);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            showRents();
+        }
+
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                toUpdate.Book = Convert.ToInt32(bookCB.Text);
+                toUpdate.Reader = Convert.ToInt32(readerCB.Text);
+                toUpdate.RentDate = DateTime.Parse(rentDateTB.Text);
+                toUpdate.ExpectedReturnDate = calculateExpectedReturnDate(DateTime.Parse(rentDateTB.Text));
+                service.UpdateRent(toUpdate);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            showAddBtn();
+            showRents();
+        }
+
+        private void rentsDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (rentsDataGridView.Columns[e.ColumnIndex].Name.Equals("return"))
+            {
+                try
+                {
+                    toUpdate = service.FindRentById((int)rentsDataGridView.Rows[e.RowIndex].Cells[0].Value);
+                    toUpdate.ReturnDate = DateTime.Now;
+                    service.UpdateRent(toUpdate);
+                    showRents();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            if (rentsDataGridView.Columns[e.ColumnIndex].Name.Equals("delete"))
+            {
+                try
+                {
+                    service.DeleteRent(service.FindRentById((int)rentsDataGridView.Rows[e.RowIndex].Cells[0].Value).Id);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+                showRents();
+            }
+
+            if (rentsDataGridView.Columns[e.ColumnIndex].Name.Equals("update"))
+            {
+                toUpdate = service.FindRentById((int)rentsDataGridView.Rows[e.RowIndex].Cells[0].Value);
+
+                bookCB.Text = toUpdate.Book.ToString();
+                readerCB.Text = toUpdate.Reader.ToString();
+                rentDateTB.Text = toUpdate.RentDate.ToString();
+
+                showUpdateBtn();
+            }
         }
     }
 }
